a2868f76bb207d94032400e5b943c26d337ae5b1
thejus-r <thejusr1999@icloud.com>
2024-12-04 17:14:07 +0530

    - day-04 Part 2

--- day-04/main.go
+++ day-04/main.go
@@ -11,7 +11,7 @@ type index struct {
 	r, c int
 }
 
-var directionsV1 = []index{
+var directionsP1 = []index{
 	{r: 0, c: 1},
 	{r: 0, c: -1},
 	{r: 1, c: 0},
@@ -22,9 +22,17 @@ var directionsV1 = []index{
 	{r: -1, c: 1},
 }
 
+var directionsP2 = []index{
+	{r: -1, c: -1},
+	{r: 1, c: 1},
+	{r: 1, c: -1},
+	{r: -1, c: 1},
+}
+
 func main() {
 	input := parseInput()
 	fmt.Printf("Part One: %d\n", partOne(input))
+	fmt.Printf("Part Two: %d\n", partTwo(input))
 
 }
 
@@ -50,7 +58,7 @@ func partOne(grid [][]rune) (wordCount int) {
 	for r := range grid {
 		for c := range grid[r] {
 			if grid[r][c] == rune(word[0]) {
-				for _, dir := range directionsV1 {
+				for _, dir := range directionsP1 {
 					findXMAS(grid, index{r: r, c: c}, dir, 0, word, &wordCount, maxRow, maxCol)
 				}
 			}
@@ -58,6 +66,45 @@ func partOne(grid [][]rune) (wordCount int) {
 	}
 
 	return wordCount
+}
+
+func partTwo(grid [][]rune) (wordCount int) {
+	const word = "MAS"
+	maxRow, maxCol := len(grid), len(grid[0])
+
+	validAIndices := make(map[index]bool)
+
+	for r := range grid {
+		for c := range grid[r] {
+			if grid[r][c] == rune(word[0]) {
+				for _, dir := range directionsP2 {
+					if findMAS(grid, index{r: r, c: c}, dir, 0, word, maxRow, maxCol) {
+						aIdx := index{r: r + dir.r, c: c + dir.c}
+						if validAIndices[aIdx] {
+							wordCount++
+						}
+						validAIndices[aIdx] = true
+					}
+				}
+			}
+		}
+	}
+	return wordCount
+}
+
+func findMAS(grid [][]rune, idx index, dir index, at int, word string, maxRow, maxCol int) bool {
+	if at+1 == len(word) {
+		return true
+	}
+
+	neigbourIndex := index{r: idx.r + dir.r, c: idx.c + dir.c}
+	if isValidIndex(neigbourIndex, maxRow, maxCol) {
+		neighbourRune := grid[neigbourIndex.r][neigbourIndex.c]
+		if neighbourRune == rune(word[at+1]) {
+			return findMAS(grid, neigbourIndex, dir, at+1, word, maxRow, maxCol)
+		}
+	}
+	return false
 }
 
 func findXMAS(grid [][]rune, idx index, dir index, at int, word string, wordCount *int, maxRow, maxCol int) {
